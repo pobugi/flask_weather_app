b599050b6fd531c30c112666afb502cfc943cb9d
pobugi <pobugi@gmail.com>
2021-05-22 15:45:36 +0500

    structure upd

--- weather_app_run.py
+++ weather_app_run.py
@@ -1,11 +1,7 @@
 """Flask Weather App"""
-from weather_app.models import City
-from weather_app import create_app, db
-from flask import Flask, request, redirect, render_template, url_for, flash
+from weather_app import create_app
 
 app = create_app()
-
-
 
 # app.run(host='0.0.0.0', debug=True)
 app.run(debug=True)
